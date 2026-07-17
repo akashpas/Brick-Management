cf3f5054824de6c6416f1313cfa15f0a06343040
akashpas <157980089+akashpas@users.noreply.github.com>
2026-07-17 09:53:24 +0530

    chore: add app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from brick_erp import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.getenv("FLASK_DEBUG", "0") == "1")
